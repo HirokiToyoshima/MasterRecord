e954f46a84724bc7a7a9672aaa89b9f8b72b3483
Hiroki Toyoshima <hir252@gmail.com>
2016-03-27 18:24:23 +0900

    add 'ttl' settings to env

--- lib/MasterRecord.rb
+++ lib/MasterRecord.rb
@@ -104,9 +104,8 @@ module MasterRecord
       self.reload if Time.current.to_i > (@data_timestamp || 0) + self.cache_seconds
     end
 
-    # TODO: キャッシュ期間を外部で管理できるようにする
     def c.cache_seconds
-      600
+      ENV["TSV_TTL_SECONDS"] ? ENV["TSV_TTL_SECONDS"].to_i : 600
     end
 
     def c.expired_at
